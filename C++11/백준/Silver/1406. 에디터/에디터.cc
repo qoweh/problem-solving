9484174b38fc2b30c26eb27b9c2d292c5820e7f0
MWK <sunnybikers@naver.com>
2025-06-23 22:03:59 +0900

    [Silver II] Title: 에디터, Time: 36 ms, Memory: 14020 KB -BaekjoonHub

--- C++11/백준/Silver/1406. 에디터/에디터.cc
+++ C++11/백준/Silver/1406. 에디터/에디터.cc
@@ -1,45 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int MX = 1000005;
+int dat[MX], pre[MX], nxt[MX];
+int unused = 1;
+
+void insert(int addr, char num){
+  dat[unused] = num;
+  nxt[unused] = nxt[addr];
+  pre[unused] = addr;
+  if (nxt[addr] != -1)
+    pre[nxt[addr]] = unused;
+  nxt[addr] = unused;
+  unused++; //?
+}
+
+void erase(int addr){
+  //addr인 주소의 노드 지우기
+  if (nxt[addr] != -1)
+    pre[nxt[addr]] = pre[addr];
+  nxt[pre[addr]] = nxt[addr];
+  dat[addr] = -1;
+}
+
+void traverse(){
+  int cur = nxt[0];
+  while(cur != -1){
+    cout << char(dat[cur]);
+    cur = nxt[cur];
+  }
+}
+
 int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  string init;
-  cin >> init;
-  list<char> L;
-  for (auto c : init) L.push_back(c);
-  auto cursor = L.end();
-  int q;
-  cin >> q;
-  while (q--)
+
+  fill(pre, pre+MX, -1);
+  fill(nxt, nxt+MX, -1);
+
+  int n, idx = 0;
+  char a, b;
+  string str;
+  cin >> str >> n;
+  for (char c : str) insert(idx++, c);
+  for (int i = 0; i < n; i++) 
   {
-    char op;
-    cin >> op;
-    switch (op)
+    cin >> a; // L | D | B | P $
+    switch (a)
     {
       case 'P':
-        char t;
-        cin >> t;
-        L.insert(cursor, t);
+        cin >> b;
+        insert(idx, b);
+        idx = nxt[idx];
         break;
       case 'L':
-        if (cursor != L.begin())
-          cursor--;
+        if (pre[idx] != -1)
+          idx = pre[idx];
         break;
       case 'D':
-        if (cursor != L.end())
-          cursor++;
+        if (nxt[idx] != -1)
+          idx = nxt[idx];
         break;
       case 'B':
-        if (cursor != L.begin())
+        if (pre[idx] != -1)
         {
-          cursor--;
-          cursor = L.erase(cursor);
+          erase(idx);
+          idx = pre[idx];
         }
         break;
     }
   }
-  for (auto c : L)
-    cout << c ;
+  traverse();
   return 0;
 }
