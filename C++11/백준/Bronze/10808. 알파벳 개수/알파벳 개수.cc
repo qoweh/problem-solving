064d27d5ed6dcf4a95cee7350c53e6c98b641989
MWK <sunnybikers@naver.com>
2025-06-23 16:13:35 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- C++11/백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ C++11/백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -1,12 +1,13 @@
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
 using namespace std;
 
 int arr['z' -'a' + 1];
 int main(void) {
-  //문자열 입력받아서 알파벳의 인덱스값 출력
-  string input;
-  cin >> input;
-  for (char c : input) arr[int(c) - int('a')]++;
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  string s;
+  cin >> s;
+  for (auto c : s) arr[c - 'a']++;
   for (int v : arr) cout << v << ' ';
   return 0;
 }
