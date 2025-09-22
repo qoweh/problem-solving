9951e156061087e29c6ccec51967480f708d4563
MWK <sunnybikers@naver.com>
2025-09-22 14:51:49 +0900

    [Silver III] Title: N과 M (7), Time: 400 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++11/백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,30 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int n, m;
+int input[7];
+int arr[7];
+
+void f(int k) {
+    if (k == m) {
+        for (int i = 0; i < k; i++) cout << arr[i] << ' ';
+        cout << '\n';
+        return ;
+    }
+    for (int i = 0; i < n; i++) {
+        arr[k] = input[i];
+        f(k + 1);
+    }
+}
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) cin >> input[i];
+    sort(input, input + n);
+    f(0);
+    return 0;
+}
