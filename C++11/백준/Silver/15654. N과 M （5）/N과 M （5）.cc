cefecbf3507fdf2930129b2c521ba8779b79cd95
MWK <sunnybikers@naver.com>
2025-09-22 14:33:12 +0900

    [Silver III] Title: N과 M (5), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- C++11/백준/Silver/15654. N과 M （5）/N과 M （5）.cc
+++ C++11/백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -27,12 +27,8 @@ int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    fill(isused, isused + 8, false);
-
     cin >> n >> m;
-    for (int i = 0; i < n; i++) {
-        cin >> input[i];
-    }
+    for (int i = 0; i < n; i++) cin >> input[i];
     sort(input, input + n);
     f(0);
     return 0;
