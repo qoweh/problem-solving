01c6c7cbfe138be94d1ce1a42560a0ed83f58f60
MWK <sunnybikers@naver.com>
2025-09-26 13:51:27 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++11/백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,36 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int n, m;
+int arr[8];
+int done[8];
+
+void f(int k) {
+    if (k == m) {
+        for (int i = 0; i < k; i++) cout << arr[done[i]] << ' ';
+        cout << '\n';
+        return ;
+    }
+    int st = 0;
+    if (k != 0) st = done[k - 1] + 1;
+    int t = 0;
+    for (int i = st; i < n; i++) {
+        if (t != arr[i]) {
+            done[k] = i;
+            t = arr[i];
+            f(k + 1);
+        }
+    }
+}
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr, arr + n);
+    f(0);
+    return 0;
+}
