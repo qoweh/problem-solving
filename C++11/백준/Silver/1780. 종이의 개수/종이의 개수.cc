8af1b0c99c9b44f94840c5f03a1ee12c69b4da97
MWK <sunnybikers@naver.com>
2025-09-28 17:50:19 +0900

    [Silver II] Title: 종이의 개수, Time: 324 ms, Memory: 20928 KB -BaekjoonHub

--- C++11/백준/Silver/1780. 종이의 개수/종이의 개수.cc
+++ C++11/백준/Silver/1780. 종이의 개수/종이의 개수.cc
@@ -1,27 +1,32 @@
 #include "bits/stdc++.h"
+
+int res[3];
+int maps[2200][2200];
+
 using namespace std;
 
-int maps[2200][2200];
-int cnt[3];
-
-bool is_same_things(int n, int a, int b) {
-    int val = maps[a][b];
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            if (maps[a + i][b + j] != val)
+bool is_same_things(int k, int x, int y) {
+    int first = maps[x][y];
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < k; j++) {
+            if (first != maps[x + i][y + j])
                 return false;
+        }
+    }
     return true;
 }
 
-void f(int n, int a, int b) {
-    if (is_same_things(n, a, b) == true) {
-        cnt[maps[a][b] + 1]++;
-        return;
+void f(int k, int x, int y) {
+    if (is_same_things(k, x, y)) {
+        res[maps[x][y] + 1]++;
+        return ;
     }
-    int third = n / 3;
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < 3; j++)
-            f(third, a + i * third, b + j * third);
+    int third = k / 3;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            f(third, x + i * third, y + j * third);
+        }
+    }
 }
 
 int main(void) {
@@ -30,11 +35,13 @@ int main(void) {
 
     int n;
     cin >> n;
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             cin >> maps[i][j];
-
+        }
+    }
     f(n, 0, 0);
-    cout << cnt[0] << '\n' << cnt[1] << '\n' << cnt[2] << '\n';
-    return 0;
+    for (int i = 0 ; i< 3; i++)
+        cout << res[i] << '\n';
+    return (0);
 }
