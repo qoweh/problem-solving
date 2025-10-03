a087dd02db56d1e265c3791406cb4cd7d205a6a7
MWK <sunnybikers@naver.com>
2025-10-03 21:57:26 +0900

    [Gold V] Title: 평범한 배낭, Time: 4 ms, Memory: 2412 KB -BaekjoonHub

--- C++11/백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ C++11/백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,23 +1,21 @@
 #include "bits/stdc++.h"
-
 using namespace std;
 
 int n, k;
-int w[105], v[105], D[105][100010];
+int w[105], v[105], D[100010];
 
-int main(void) {
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     cin >> n >> k;
     for (int i = 1; i <= n; i++) cin >> w[i] >> v[i];
-
     for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= k; j++) {
-            D[i][j] = D[i-1][j];
-            if (j >= w[i]) D[i][j] = max(D[i][j], D[i-1][j -w[i]] + v[i]);
+        for (int cur_w = k; cur_w >= w[i]; --cur_w) {
+            D[cur_w] = max(D[cur_w], D[cur_w - w[i]] + v[i]);
         }
     }
-    cout << D[n][k];
-    return (0);
+
+    cout << D[k];
+    return 0;
 }
