970c080b9f631cfe73787503fc79a72978da0870
MWK <sunnybikers@naver.com>
2025-10-03 20:28:38 +0900

    [Gold V] Title: 회의실 배정, Time: 28 ms, Memory: 2804 KB -BaekjoonHub

---
+++ C++11/백준/Gold/1931. 회의실 배정/회의실 배정.cc
@@ -0,0 +1,34 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int n, res;
+pair<int, int> t[100010];
+
+bool second_fast(pair<int, int> p1, pair<int, int> p2) {
+    if (p1.second != p2.second) return p1.second < p2.second;
+    return p1.first < p2.first;
+}
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        t[i] = make_pair(a, b);
+    }
+    sort(t, t + n, second_fast);
+    
+    int pre_end_time = 0;
+    for (int i = 0; i < n; i++) {
+        if (t[i].first >= pre_end_time) {
+            res++;
+            pre_end_time = t[i].second;
+        }
+    }
+    cout << res;
+    return (0);
+}
