510ccee49495c349033f93d618afd3ea5b691af2
MWK <sunnybikers@naver.com>
2025-10-06 18:10:41 +0900

    [Gold IV] Title: 오큰수, Time: 188 ms, Memory: 8468 KB -BaekjoonHub

--- C++11/백준/Gold/17298. 오큰수/오큰수.cc
+++ C++11/백준/Gold/17298. 오큰수/오큰수.cc
@@ -2,23 +2,21 @@
 
 using namespace std;
 
-int a[1000000];
-int ans[1000000];
-
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++) cin >> a[i];
+    int n, s[1000010];
     stack<int> S;
-    for (int i = n - 1; i >= 0; i--) {
-        while (!S.empty() && S.top() <= a[i]) S.pop();
-        if (S.empty()) ans[i] = -1;
-        else ans[i] = S.top();
-        S.push(a[i]);
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> s[i];
+
+    for (int i = n-1; i>=0; i--) {
+        int k = s[i];
+        while (!S.empty() && S.top() <= k) S.pop();
+        s[i] = S.empty() ? -1 : S.top();
+        S.push(k);
     }
-    for (int i = 0; i < n; i++) cout << ans[i] << ' ';
-    return 0;
+    for (int i = 0; i < n; i++) cout << s[i] << ' ';
+    return (0);
 }
