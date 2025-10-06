7356b235f054e183c7738ed13b8b600b941b563f
MWK <sunnybikers@naver.com>
2025-10-06 17:08:27 +0900

    [Gold V] Title: 탑, Time: 92 ms, Memory: 6300 KB -BaekjoonHub

--- C++11/백준/Gold/2493. 탑/탑.cc
+++ C++11/백준/Gold/2493. 탑/탑.cc
@@ -2,22 +2,19 @@
 
 using namespace std;
 
-stack<pair<int, int> > S;
-
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
-    int n;
-    cin >> n;
+    
+    int n, cur;
+    stack<pair<int, int> > S;
     S.push(make_pair(100000001, 0));
-    for (int N = 1; N <= n; N++) {
-        int cur;
+    cin >> n;
+    for (int i = 0 ; i < n; i++) {
         cin >> cur;
-        while (S.top().first < cur)
-            S.pop(); // 가장 큰 값 100000001가 있기에 S.pop()에서 에러 안 남.
+        while (S.top().first < cur) S.pop();
         cout << S.top().second << ' ';
-        S.push(make_pair(cur, N));
+        S.push(make_pair(cur, i + 1));
     }
     return (0);
 }
