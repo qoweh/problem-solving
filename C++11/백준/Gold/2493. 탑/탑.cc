747e068cb464960dd541225d5306dfa9259eadf9
MWK <sunnybikers@naver.com>
2025-10-06 17:12:22 +0900

    [Gold V] Title: 탑, Time: 88 ms, Memory: 6300 KB -BaekjoonHub

--- C++11/백준/Gold/2493. 탑/탑.cc
+++ C++11/백준/Gold/2493. 탑/탑.cc
@@ -6,15 +6,20 @@ int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    int n, cur;
+    int n, pre = 100000001, cur;
     stack<pair<int, int> > S;
     S.push(make_pair(100000001, 0));
     cin >> n;
     for (int i = 0 ; i < n; i++) {
         cin >> cur;
-        while (S.top().first < cur) S.pop();
+        if (pre > cur) {
+            // cout << S.top().second << ' ';
+        } else {
+            while (S.top().first < cur) S.pop();
+        }
         cout << S.top().second << ' ';
         S.push(make_pair(cur, i + 1));
+        pre = cur;
     }
     return (0);
 }
