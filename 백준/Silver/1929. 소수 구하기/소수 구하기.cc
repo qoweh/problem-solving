5ce38d97d4c953aaa13d704fe89c75fcb5855cc8
MWK <sunnybikers@naver.com>
2026-01-13 20:47:42 +0900

    [Silver III] Title: 소수 구하기, Time: 20 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -0,0 +1,29 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+vector<bool> V(1000010, true);
+
+void f(int last) {
+	for (int i = 2; i < last; i++) {
+		for (int j = i + i; j <= last; j += i) {
+			V[j] = false;
+		}
+	}
+}
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+	V[1] = false;
+
+    int m, n;
+	cin >> m >> n;
+
+	f(n);
+	
+	for (int i = m; i <= n; i++)
+		if (V[i]) cout << i << '\n';
+    return (0);
+}
