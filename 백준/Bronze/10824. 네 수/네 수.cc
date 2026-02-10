c6564e5f430db7107b9a296f081a73e7d37f4db7
MWK <sunnybikers@naver.com>
2026-02-10 21:41:25 +0900

    [Bronze III] Title: 네 수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10824. 네 수/네 수.cc
+++ 백준/Bronze/10824. 네 수/네 수.cc
@@ -2,27 +2,6 @@
 
 using namespace std;
 
-long long string_to_int(string s) {
-	long long t = 0;
-
-	if (s.size() < 1) return -1;
-	for (int i = 0; i < s.size(); i++) {
-		t = t * 10 + s[i] - '0';
-	}
-	return t;
-}
-
-long long f(string a, string b, string c, string d) {
-	long long t = 0;
-
-	string ab = a + b;
-	string cd = c + d;
-
-	t += string_to_int(ab);
-	t += string_to_int(cd);
-	return t;
-}
-
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
@@ -30,6 +9,6 @@ int main() {
 	string a, b, c, d;
 
 	cin >> a >> b >> c >> d;
-	cout <<f(a, b, c, d);
+	cout << stoll(a + b) + stoll(c + d);
 	return 0;
 }
