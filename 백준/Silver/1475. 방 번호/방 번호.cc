5302cb78c8fccc88191601b3cb9935d5f87c282c
MWK <sunnybikers@naver.com>
2026-02-16 22:19:15 +0900

    [Silver V] Title: 방 번호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.cc
+++ 백준/Silver/1475. 방 번호/방 번호.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int s[10];
@@ -12,11 +13,7 @@ int f(int& k) {
 	s[6] = s[9] = (s[6] + s[9] + 1) / 2; 
 	// 1을 더해줘서 s[6]과 s[9]의 합이 홀수일 때 반올림한다.
 
-	int res = s[0];
-	for (int i = 1; i < 10; i++) {
-		if (res < s[i]) res = s[i];
-	}
-	return res;
+	return *max_element(s, s + 10);
 }
 
 int main() {
