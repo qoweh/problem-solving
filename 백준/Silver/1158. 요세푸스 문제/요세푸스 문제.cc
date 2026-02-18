1d349868bcdd19766647f6129c562004251ee306
MWK <sunnybikers@naver.com>
2026-02-18 19:49:10 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 592 ms, Memory: 2284 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+list<int> L;
+
+void f(int& step, list<int>::iterator& iter) {
+	while (!L.empty()) {
+		int k = step;
+		while (k-- > 0) {
+			L.push_back(L.front());
+			L.pop_front();
+		}
+		cout << L.back();
+		if (L.size() != 1) cout << ", ";
+		L.pop_back();
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n, k;
+	cin >> n >> k;
+	for (int i = 1; i <= n; i++) L.push_back(i);
+
+	list<int>::iterator iter = L.begin();
+	cout << '<';
+	f(k, iter);
+	cout << '>';
+	return 0;
+}
