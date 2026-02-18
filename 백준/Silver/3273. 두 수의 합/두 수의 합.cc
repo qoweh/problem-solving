aaee78283a06dfd4c280d39dc014632ec7e16020
MWK <sunnybikers@naver.com>
2026-02-18 18:44:21 +0900

    [Silver III] Title: 두 수의 합, Time: 4 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -1,24 +1,27 @@
-#include "bits/stdc++.h"
-
+#include <iostream>
 using namespace std;
 
-int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+bool s[2000010];
 
-    int n, x;
-    bool s[2000010] = {false};
-    cin >> n;
-    for (int i = 0 ; i < n; i++) {
-        int k;
-        cin >> k;
-        s[k] = true;
-    }
-    cin >> x;
-    int res = 0;
-    for (int i = 1; i <= (x-1)/2; i++) {
-        if (s[i] && s[x-i]) res++;
-    }
-    cout << res;
-    return (0);
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n, t, x, res = 0;
+
+	fill(s, s + 2000010, false);
+
+	cin >> n;
+	while (n-- > 0) {
+		cin >> t;
+		s[t] = true;
+	}
+	cin >> x;
+
+	for (int i = 1; i <= ((x+1) / 2) - 1; i++) {
+		if (s[i] && s[x-i])
+			res++;
+	}
+	cout << res;
+	return 0;
 }
