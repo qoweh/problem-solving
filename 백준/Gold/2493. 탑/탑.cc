b17224343c36fa28a395a287bbbba3890ed1faa9
MWK <sunnybikers@naver.com>
2026-02-21 15:28:38 +0900

    [Gold V] Title: 탑, Time: 88 ms, Memory: 8132 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -3,28 +3,26 @@
 
 using namespace std;
 
-stack<int> s;
+// stack<int> s;
+stack<pair<int, int>> s;
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
 	int n;
-	int arr[500010] = {0};
 	int res[500010];
 	cin >> n;
-	for (int i = 1; i <= n; i++) {
-		cin >> arr[i]; // 탑의 높이는 서로 다름.
-	}
-
 
 	// stack의 역할 : 특정 위치의 왼쪽에서 볼 수 있는 탑의 후보들을 담는 버퍼
 	for (int i = 1; i <= n; i++) {
-		while (!s.empty() && arr[s.top()] < arr[i]) {
+		int h;
+		cin >> h;
+		while (!s.empty() && s.top().second < h) {
 			s.pop();
 		}
-		res[i] = (s.empty()) ? 0 : s.top(); // s에 push하는 것은 index임.
-		s.push(i);
+		res[i] = (s.empty()) ? 0 : s.top().first; // s에 push하는 것은 index임.
+		s.push({i, h});
 	}
 
 	for (int i = 1; i <= n; i++) cout << res[i] << ' ';	 
