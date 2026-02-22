9192bc840bc3218f614d28ff0d83acfdd5612763
MWK <sunnybikers@naver.com>
2026-02-23 00:55:08 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.cpp
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.cpp
@@ -4,12 +4,14 @@
 using namespace std;
 
 int solution(int chicken) {
+    return (chicken-1)/9;
+    
+    
     int res = 0, coupon = chicken;
     while (chicken > 0) {
         res += coupon / 10;
         coupon = coupon / 10 + coupon % 10;      
         chicken /= 10;
     }
-    res += coupon / 10;
     return res;
 }
