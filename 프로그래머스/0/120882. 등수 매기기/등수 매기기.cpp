1b41c90d74e3ffa4d90fd41e01bd9b442f3a139c
MWK <sunnybikers@naver.com>
2026-02-22 19:14:14 +0900

    [level 0] Title: 등수 매기기, Time: 0.02 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.cpp
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.cpp
@@ -5,10 +5,28 @@
 
 using namespace std;
 
+vector<int> solution(vector<vector<int>> score) {
+    vector<int> t, answer;
+
+    for (auto& c : score) t.push_back(c.front() + c.back());
+    sort(t.begin(), t.end(), greater<>());
+    
+    for (auto& c : score) {
+        int k = c.front() + c.back();
+        int idx = find(t.begin(), t.end(), k) - t.begin() + 1; 
+        answer.push_back(idx);
+    }
+    
+    return answer;
+}
+
+
+
+
 int arr[201];
 map<int, int> m;
 
-vector<int> solution(vector<vector<int>> score) {
+vector<int> solution1(vector<vector<int>> score) {
     vector<int> t, answer;
     for (auto& c : score) arr[(c[0] + c[1])]++;
     for (int i = 200, cnt = 1; i >= 0; i--) {
