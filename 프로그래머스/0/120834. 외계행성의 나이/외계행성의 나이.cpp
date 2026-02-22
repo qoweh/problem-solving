2c94fb4e263a05c3afc13e2f5cc001e122d2ea10
MWK <sunnybikers@naver.com>
2026-02-22 13:58:56 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.cpp
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.cpp
@@ -1,13 +1,20 @@
 #include <string>
-#include <vector>
+#include <stack>
 
 using namespace std;
 
+stack<int> s;
+
 string solution(int age) {
     string answer = "abcdefghij";
+    while (age > 0) {
+        s.push(age % 10);
+        age /= 10;
+    }
     string res = "";
-    for (auto& c : to_string(age)) {
-        res += answer[c - '0'];
+    while (!s.empty()) {
+        res += answer[s.top()];
+        s.pop();   
     }
     return res;
 }
