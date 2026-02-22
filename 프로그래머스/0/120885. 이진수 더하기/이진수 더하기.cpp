c95f2c514d13b0e5e9dd7fb45c2ef8cd36c122a0
MWK <sunnybikers@naver.com>
2026-02-23 01:21:17 +0900

    [level 0] Title: 이진수 더하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.cpp
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.cpp
@@ -20,3 +20,19 @@ string solution(string b1, string b2) {
     reverse(result.begin(), result.end());
     return result;
 }
+
+
+string solution2(string b1, string b2) {
+    int n1 = stoi(b1, nullptr, 2);
+    int n2 = stoi(b2, nullptr, 2);
+
+    int sum = n1 + n2;
+
+    string res = "";
+    while (sum > 0) {
+        res = char(sum % 2 + '0') + res;
+        sum /= 2;
+    }
+
+    return res == "" ? "0" : res;
+}
