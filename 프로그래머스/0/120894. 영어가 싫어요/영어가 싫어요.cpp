3fb68fe74d409324374d9e264f63bdf32efc37e6
MWK <sunnybikers@naver.com>
2026-02-23 14:25:52 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 3.75 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.cpp
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.cpp
@@ -1,30 +1,22 @@
 #include <string>
-#include <map>
+#include <vector>
 
 using namespace std;
 
-map<string, int> m = {
-    {"zero", 0},
-    {"one", 1},
-    {"two", 2}, 
-    {"three", 3}, 
-    {"four", 4}, 
-    {"five", 5}, 
-    {"six", 6}, 
-    {"seven", 7}, 
-    {"eight", 8}, 
-    {"nine", 9}
-};
-
 long long solution(string numbers) {
-    long long answer = 0;
-    string s = "";
-    for (auto& c : numbers) {
-        s += c;
-        if (m.find(s) != m.end()) {
-            answer = 10 * answer + m[s];
-            s.clear();
+    vector<string> v = {
+        "zero","one","two","three","four",
+        "five","six","seven","eight","nine"
+    };
+
+    int i = 0;
+    for (auto& s : v) {
+        int idx;
+        while ((idx = numbers.find(s)) != string::npos) {
+            numbers.replace(idx, s.size(), to_string(i));
         }
+        i++;
     }
-    return answer;
+
+    return stoll(numbers);
 }
